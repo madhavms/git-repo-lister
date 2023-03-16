dc306a5eb126d0f125ea60820532a1465a13ec58
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-16 01:20:06 +0000

    Style changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import {getRepos} from './utilities/getRepos'
+import { getRepos } from './utilities/getRepos';
 import './App.css';
 import { List } from './components/List';
 import { useState, useEffect } from 'react';
 import { Loader } from './components/Loader';
 
-const DEFAULT_USER = 'facebook'
+const DEFAULT_USER = 'facebook';
 
 function App() {
-
   const [repos, setRepos] = useState([]);
   const [user, setUser] = useState(DEFAULT_USER);
   const [text, setText] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-  
+
   useEffect(() => {
-    getRepos({username:user, setIsLoading}).then(data => setRepos(data));
-  },[user])
+    getRepos({ username: user, setIsLoading }).then((data) => setRepos(data));
+  }, [user]);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(text)
-    if(text){
+    if (text) {
       setUser(text);
       setIsLoading(true);
     }
-  }
+  };
 
   return (
     <div className="App">
       <h1>Repos for user: {user}</h1>
-      <form onSubmit={handleFormSubmit}>
-      <input placeholder="Enter username" onChange={e => setText(e.target.value)}/>
-      &nbsp;
-      <button type="submit" className='btn btn-primary' disabled={!text}>Submit</button>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          className="form-input"
+          placeholder="Enter username"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary form-button"
+          disabled={!text}
+        >
+          Submit
+        </button>
       </form>
       <Loader isLoading={isLoading}>
-      <List repos={repos} isLoading={isLoading}/>
+        <List repos={repos} isLoading={isLoading} />
       </Loader>
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,18 @@
+import '../styles/List.css';
+
 export const List = (props) => {
-    const {repos, isLoading} = props;
-    if(!repos) return null;
-    if(!repos.length && !isLoading) return <p>Sorry, no items found</p>
-    return(
-        <ul>
-        {repos.map(repo => <li key={repo.id}><b>{repo.name}</b> (⭐: {repo.stars})<br/><i>{repo.description}</i><br/><br/></li>)}
-        </ul>
-    )
+  const {repos, isLoading} = props;
+  if(!repos) return null;
+  if(!repos.length && !isLoading) return <p>Sorry, no items found</p>
+  return(
+    <ul className="list">
+      {repos.map(repo => 
+        <li key={repo.id} className="list-item">
+          <h3 className="list-title"><b>{repo.name}</b></h3>
+          <p className="list-stars">⭐ {repo.stars}</p>
+          <p className="list-description">{repo.description}</p>
+        </li>
+      )}
+    </ul>
+  )
 }
